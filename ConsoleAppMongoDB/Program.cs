cda39af546ca9d9816005a8ba6eed0286ff632ac
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-09-20 15:58:17 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleAppMongoDB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MongoDB.Driver;
+
+Console.WriteLine("Hola Mundo, VS2026 + MongoDB");
+
+var mongoUrl = Environment.GetEnvironmentVariable("MONGODB_URI");
+var client = new MongoClient(mongoUrl);
+
+Console.WriteLine("Conectando a MongoDB...");
+var dbList = client.ListDatabaseNames().ToList();
+
+Console.WriteLine("Conexión establecida con MongoDB.");
+
+Console.WriteLine("Bases de datos disponibles:");
+foreach (var dbName in dbList)
+{
+    Console.WriteLine($" - {dbName}");
+}
