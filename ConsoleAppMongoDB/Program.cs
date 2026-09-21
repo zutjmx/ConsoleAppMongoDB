2db93aa99ab59499f74064036853a8e02d4bc77f
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-09-21 17:00:04 -0600

    Querying a MongoDB Collection in C# Applications

--- ConsoleAppMongoDB/Program.cs
+++ ConsoleAppMongoDB/Program.cs
@@ -22,6 +22,23 @@ foreach (var dbName in dbList)
 Console.WriteLine("Nos aseguramos de tener la base de datos y colección correctas:");
 var database = client.GetDatabase("test");
 
+// Querying a MongoDB Collection in C# Applications
+
+var accountsCollection = database.GetCollection<Account>("account");
+
+var account = accountsCollection
+   .Find(a => a.AccountId == "MDB829001337")
+   .FirstOrDefault();
+
+Console.WriteLine($"Cuenta encontrada: {account?.AccountHolder}, Balance: {account?.Balance}");
+
+Console.WriteLine("Lista de cuentas:");
+var accounts = accountsCollection.Find(_ => true).ToList();
+foreach (var item in accounts)
+{
+    Console.WriteLine(item.AccountHolder);
+}
+
 // Usando la clase Account para representar un documento de cuenta bancaria
 //var accountsCollection = database.GetCollection<Account>("account");
 //Console.WriteLine("Creando una nueva cuenta...");
@@ -38,16 +55,16 @@ var database = client.GetDatabase("test");
 //Console.WriteLine("Nueva cuenta insertada en la colección.");
 
 // Usando el objeto BsonDocument para insertar un documento directamente
-var accountsCollectionBson = database.GetCollection<BsonDocument>("account");
-Console.WriteLine("Creando una nueva cuenta...");
-var document = new BsonDocument
-{
-   { "account_id", "MDB829001338" },
-   { "account_holder", "Ada Lovelace" },
-   { "account_type", "checking" },
-   { "balance", 79352434 }
-};
+//var accountsCollectionBson = database.GetCollection<BsonDocument>("account");
+//Console.WriteLine("Creando una nueva cuenta...");
+//var document = new BsonDocument
+//{
+//   { "account_id", "MDB829001338" },
+//   { "account_holder", "Ada Lovelace" },
+//   { "account_type", "checking" },
+//   { "balance", 79352434 }
+//};
 
-Console.WriteLine("Insertando la nueva cuenta en la colección...");
-accountsCollectionBson.InsertOne(document);
-Console.WriteLine("Nueva cuenta insertada en la colección.");
+//Console.WriteLine("Insertando la nueva cuenta en la colección...");
+//accountsCollectionBson.InsertOne(document);
+//Console.WriteLine("Nueva cuenta insertada en la colección.");
