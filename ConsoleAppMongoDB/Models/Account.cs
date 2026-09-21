eca1f85ccf2fc1f34539035b32c806e20c096771
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-09-21 16:33:53 -0600

    Inserting a Document in C# Applications

---
+++ ConsoleAppMongoDB/Models/Account.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ConsoleAppMongoDB.Models
+{
+    internal class Account
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("account_id")]
+        public string AccountId { get; set; } = string.Empty;
+
+        [BsonElement("account_holder")]
+        public string AccountHolder { get; set; } = string.Empty;
+
+        [BsonElement("account_type")]
+        public string AccountType { get; set; } = string.Empty;
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        [BsonElement("balance")]
+        public decimal Balance { get; set; }
+
+        [BsonElement("transfers_complete")]
+        public string[] TransfersCompleted { get; set; } = Array.Empty<string>();
+    }
+}
--- ConsoleAppMongoDB/Program.cs
+++ ConsoleAppMongoDB/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using ConsoleAppMongoDB.Models;
 
 Console.WriteLine("Hola Mundo, VS2026 + MongoDB");
 
@@ -16,3 +17,37 @@ foreach (var dbName in dbList)
 {
     Console.WriteLine($" - {dbName}");
 }
+
+
+Console.WriteLine("Nos aseguramos de tener la base de datos y colección correctas:");
+var database = client.GetDatabase("test");
+
+// Usando la clase Account para representar un documento de cuenta bancaria
+//var accountsCollection = database.GetCollection<Account>("account");
+//Console.WriteLine("Creando una nueva cuenta...");
+//var newAccount = new Account
+//{
+//    AccountId = "MDB829001337",
+//    AccountHolder = "Linus Torvalds",
+//    AccountType = "checking",
+//    Balance = 50352434
+//};
+
+//Console.WriteLine("Insertando la nueva cuenta en la colección...");
+//accountsCollection.InsertOne(newAccount);
+//Console.WriteLine("Nueva cuenta insertada en la colección.");
+
+// Usando el objeto BsonDocument para insertar un documento directamente
+var accountsCollectionBson = database.GetCollection<BsonDocument>("account");
+Console.WriteLine("Creando una nueva cuenta...");
+var document = new BsonDocument
+{
+   { "account_id", "MDB829001338" },
+   { "account_holder", "Ada Lovelace" },
+   { "account_type", "checking" },
+   { "balance", 79352434 }
+};
+
+Console.WriteLine("Insertando la nueva cuenta en la colección...");
+accountsCollectionBson.InsertOne(document);
+Console.WriteLine("Nueva cuenta insertada en la colección.");
